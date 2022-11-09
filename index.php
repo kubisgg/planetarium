7e1f9422b57f9ebe268450e16e1e637dd414869f
kubisgg <andrzejjjestem@gmail.com>
2022-11-10 00:13:05 +0100

    eo

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Planetarium - Strona Główna</title>
+
+    <link rel="stylesheet" href="/planetarium/css/style.css" class="stylesheet">
+    <link rel="stylesheet" href="/planetarium/css/header.css" class="stylesheet">
+    <link rel="stylesheet" href="/planetarium/css/footer.css" class="stylesheet">
+
+    <!-- Font Awesome 6.2 -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+</head>
+<body>
+
+    <div id="container">
+
+        <?php require 'templates/header.php' ?>
+
+        <div class="content">
+            asd
+        </div>
+
+    </div>
+
+
+
+</body>
+</html>
---
+++ templates/header.php
@@ -0,0 +1,15 @@
+<div id="header">
+
+    <h1>Planetarium</h1>
+
+    <div class="login-buttons">
+
+        <a href="login.php"><button class="login-button"><i class="fa-solid fa-right-to-bracket"></i> Zaloguj</button></a>
+
+        <a href="register.php"><button class="login-button"><i class="fa-solid fa-clipboard-check"></i> Zarejestruj</button></a>
+
+    </div>
+
+    <!-- <p>asd</p> -->
+
+</div>
